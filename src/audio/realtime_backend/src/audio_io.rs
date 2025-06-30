1f1ca5d30558380a0951210c3d5ee882867857bb
abehlok2 <132627628+abehlok2@users.noreply.github.com>
2025-06-29 23:13:37 -0400

    Add stop mechanism for realtime audio thread

--- src/audio/realtime_backend/src/audio_io.rs
+++ src/audio/realtime_backend/src/audio_io.rs
@@ -2,10 +2,11 @@ use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 use cpal::{SampleFormat, StreamConfig};
 use parking_lot::Mutex;
 use std::sync::Arc;
+use crossbeam::channel::Receiver;
 
 use crate::scheduler::TrackScheduler;
 
-pub fn run_audio_stream(scheduler: Arc<Mutex<TrackScheduler>>) {
+pub fn run_audio_stream(scheduler: Arc<Mutex<TrackScheduler>>, stop_rx: Receiver<()>) {
     let host = cpal::default_host();
     let device = host
         .default_output_device()
@@ -28,12 +29,11 @@ pub fn run_audio_stream(scheduler: Arc<Mutex<TrackScheduler>>) {
     };
     stream.play().unwrap();
 
-    // Keep the stream alive until the thread is killed
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(100));
-    }
+    // Keep the stream alive until a stop signal is received
+    while stop_rx.recv_timeout(std::time::Duration::from_millis(100)).is_err() {}
 }
 
-pub fn stop_audio_stream() {
-    // TODO: Implement a mechanism to stop the audio thread
+// The actual stop logic is handled via the channel in `run_audio_stream`.
+pub fn stop_audio_stream(sender: &crossbeam::channel::Sender<()>) {
+    let _ = sender.send(());
 }
