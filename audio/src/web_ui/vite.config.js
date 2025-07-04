72f022576d3ea075b55c3c47e39c77bda8a7e9f6
alexbehlok <abehlok2@wgu.edu>
2025-07-04 01:07:46 -0400

    fixed some browser sec issue blocking the high efficiency ring buffer

--- audio/src/web_ui/vite.config.js
+++ audio/src/web_ui/vite.config.js
@@ -3,8 +3,17 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     open: true,
+    // Add these headers to enable SharedArrayBuffer
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+    },
   },
   build: {
     outDir: 'dist',
   },
+  // Optimize dependencies to prevent vite from reloading the page on wasm changes
+  optimizeDeps: {
+    exclude: ['/src/pkg/realtime_backend.js?import']
+  }
 });
